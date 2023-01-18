fd50b5e5b9d50cbda6cb647ae3c3ae711a0b3444
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-18 20:10:16 -0300

    :pick: Fix names of functions

--- XForce/details/__init__.py
+++ XForce/details/__init__.py
@@ -1,26 +1,26 @@
 from requests import get
 
-api_url = 'https://api.xforce.ibmcloud.com/api'
+api_url = "https://exchange.xforce.ibmcloud.com/api" 
 
-def get_activities(activity_id, auth): 
+def activity(activity_id, auth): 
     data = eval(get(f'{api_url}/threat_activities/{activity_id}',
                headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/threats/{activity_id}'
     return data
 
-def get_malware(malware_id, auth): 
+def malware(malware_id, auth): 
     data = eval(get(f'{api_url}/malware_analysis/{malware_id}',
                headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/malware_analysis/{malware_id}'
     return data
 
-def get_groups(groups_id, auth): 
+def group(groups_id, auth): 
     data = eval(get(f'{api_url}/threat_groups/{groups_id}',
                headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/threat-group/{groups_id}'
     return data
 
-def get_industries(industries_id, auth): 
+def industry(industries_id, auth): 
     data = eval(get(f'{api_url}/industries/{industries_id}',
                headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/industries/{industries_id}'
