6bc3da28b3656b75e6566c0b254537ce7bff385a
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-18 18:23:31 -0300

    :tada: Refatoring and make function for get all results

---
+++ X-FORCE/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3 
+from requests import get
+
+api_url = "https://exchange.xforce.ibmcloud.com/api" 
+
+# Sources of search
+def threat_activities(term, auth):
+    return get(f"{api_url}/threat_activities/search/{term}",
+               headers={ 'Authorization': f'Basic {auth}' }).text
+
+def malware_analysis(term, auth):
+    return get(f"{api_url}/malware_analysis/search/{term}",
+               headers={ 'Authorization': f'Basic {auth}' }).text
+
+def threat_groups(term, auth):
+    return get(f"{api_url}/threat_groups/search/{term}",
+               headers={ 'Authorization': f'Basic {auth}' }).text
+
+def casesfiles(term, auth):
+    return get(f"{api_url}/casefiles/public/fulltext?q={term}",
+               headers={ 'Authorization': f'Basic {auth}' }).text
+
+def industries(term, auth):
+    return get(f"{api_url}/industries/search/{term}",
+               headers={ 'Authorization': f'Basic {auth}' }).text
+
+def all(term, auth):
+    big_list = []
+    big_list.append(eval(threat_activities(term, auth))["rows"])
+    big_list.append(eval(malware_analysis(term, auth))["rows"])
+    big_list.append(eval(threat_groups(term, auth))["rows"])
+    big_list.append(eval(casesfiles(term, auth))["rows"])
+    big_list.append(eval(industries(term, auth))["rows"])
+    return big_list
+
+breakpoint()
