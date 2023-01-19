6a3e6646337ee1e0cfbc0c2989142fe1cc212fda
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-18 22:37:32 -0300

    :pick: Update version python and fix category

--- setup.py
+++ setup.py
@@ -5,25 +5,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='XForce',
-    version=0.1,
+    version=0.2,
     author="Julio Lira",
     author_email="jul10l1r4@disroot.org",
     description="IBM Security utilitary library in python. Search and query all sources: threat_activities and groups, malware_analysis, industries ",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jul10l1r4/x-force",
-    packages=["XForce", "XForce.industries", "XForce.malware_analysis", "XForce.threat_activities", "XForce.threat_groups",
-              "XForce.details.industry", "XForce.details.malware", "XForce.details.activity", 
-             "XForce.details.group"],
+    packages=["XForce", "XForce", "XForce.details"],
     license="GPLv3",
     keywords="threat intelligence security ibm xforce x-force blueteam search query api exchange otx cti oct",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
-        "Topic :: Security",
-        "Security :: Threat Intelligence",
         "Topic :: Internet",
+        "Topic :: Security",
         ],
     )
 
