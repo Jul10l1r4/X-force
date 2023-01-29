57ff14b6710c8205eedbfd34bee8eb8f185ccb3a
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-29 14:49:34 -0300

    :rocket: update fix version of 0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='XForce',
-    version=0.6,
+    version=0.6.1,
     author="Julio Lira",
     author_email="jul10l1r4@disroot.org",
     description="IBM Security utilitary library in python. Search and query all sources: threat_activities and groups, malware_analysis, industries ",
