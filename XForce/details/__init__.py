7ec00e9475a75a35c22a292c4944066e01c8a59b
jul10l1r4 <jul10l1r4@disroot.org>
2023-01-29 22:34:33 -0300

    :pick: Removing eval of death

--- XForce/details/__init__.py
+++ XForce/details/__init__.py
@@ -4,31 +4,31 @@ from json import loads
 api_url = "https://exchange.xforce.ibmcloud.com/api" 
 
 def activity(activity_id, auth): 
-    data = loads(eval(get(f'{api_url}/threat_activities/{activity_id}',
-               headers={ 'Authorization': f'Basic {auth}' }).text))
+    data = loads(get(f'{api_url}/threat_activities/{activity_id}',
+               headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/threats/{activity_id}'
     return data
 
 def malware(malware_id, auth): 
-    data = loads(eval(get(f'{api_url}/malware_analysis/{malware_id}',
-               headers={ 'Authorization': f'Basic {auth}' }).text))
+    data = loads(get(f'{api_url}/malware_analysis/{malware_id}',
+               headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/malware_analysis/{malware_id}'
     return data
 
 def group(groups_id, auth): 
-    data = loads(eval(get(f'{api_url}/threat_groups/{groups_id}',
-               headers={ 'Authorization': f'Basic {auth}' }).text))
+    data = loads(get(f'{api_url}/threat_groups/{groups_id}',
+               headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/threat-group/{groups_id}'
     return data
 
 def collector(collector_id, auth): 
-    data = loads(eval(get(f'{api_url}/casefiles/{collector_id}',
-               headers={ 'Authorization': f'Basic {auth}' }).text))
+    data = loads(get(f'{api_url}/casefiles/{collector_id}',
+               headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/collector/{collector_id}'
     return data
 
 def industry(industries_id, auth): 
-    data = loads(eval(get(f'{api_url}/industries/{industries_id}',
-               headers={ 'Authorization': f'Basic {auth}' }).text))
+    data = loads(get(f'{api_url}/industries/{industries_id}',
+               headers={ 'Authorization': f'Basic {auth}' }).text)
     data['url'] = f'https://exchange.xforce.ibmcloud.com/industries/{industries_id}'
     return data
